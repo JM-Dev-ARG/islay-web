76070a4bbf55483de782876e149bb9785de37569
JM-Dev <jtogni.developer@gmail.com>
2024-11-19 15:02:04 -0300

    cambios en mailer

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,12 +1,8 @@
 import nodemailer from "nodemailer";
 
-
-
-
 export const transporter = nodemailer.createTransport({
   host: import.meta.env.SMTP_SERVER,
-  port: 465,
-  secure: true,
+  port: 587,
   auth: {
     user: import.meta.env.MAIL_USER,
     pass: import.meta.env.MAIL_PASS,
